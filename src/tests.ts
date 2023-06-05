9cb49f450e5c40a19bd501471d847dfb6ec2d3b4
peron <peron_ap@icloud.com>
2023-06-05 20:56:18 +0800

    tests: add test cases

---
+++ src/tests.ts
@@ -0,0 +1,78 @@
+import { connect } from "https://deno.land/x/redis@v0.29.4/mod.ts";
+import { FakeTime } from "https://deno.land/std@0.190.0/testing/time.ts";
+import {
+  assert,
+  assertEquals,
+} from "https://deno.land/std@0.190.0/testing/asserts.ts";
+import { RateLimiter } from "./limiter.ts";
+
+const redis = await connect({
+  hostname: "127.0.0.1",
+  port: 6379,
+});
+
+await redis.del("ratelimit:test:test");
+
+const rateLimiter = new RateLimiter({
+  keyPrefix: "ratelimit:test",
+  windowMs: 60000,
+  maxRequests: 10,
+  async evalFunc(script, keys, args) {
+    const raw = await redis.eval(script, keys, args);
+    return raw!.toString();
+  },
+  async delFunc(key) {
+    await redis.del(key);
+  },
+});
+
+Deno.test("ratelimit", async () => {
+  const time = new FakeTime();
+
+  try {
+    for (let i = 0; i < 10; ++i) {
+      const limitResult = await rateLimiter.limit("test");
+
+      // should not be limited
+      assertEquals(limitResult, {
+        isLimited: false,
+        remainingRequests: 9 - i,
+        nextAvailableTimestamp: time.start + 60000,
+      });
+
+      const checkResult = await rateLimiter.check("test");
+
+      if (i === 9) {
+        // check should return limited after max requests exceeded
+        assert(checkResult.isLimited);
+      } else {
+        // check should return the same result
+        assertEquals(checkResult, limitResult);
+      }
+
+      time.tick(1000);
+    }
+
+    // further requests should be limited
+    const shouldLimit = await rateLimiter.limit("test");
+    assertEquals(shouldLimit, {
+      isLimited: true,
+      remainingRequests: 0,
+      nextAvailableTimestamp: time.start + 60000,
+    });
+
+    time.tick(50000);
+
+    // after window expires, requests should no longer be limited
+    const shouldNotLimit = await rateLimiter.limit("test");
+    assertEquals(shouldNotLimit, {
+      isLimited: false,
+      remainingRequests: 0,
+      nextAvailableTimestamp: time.start + 61000,
+    });
+  } finally {
+    // clean up
+    time.restore();
+    await redis.del("ratelimit:test:test");
+  }
+});
