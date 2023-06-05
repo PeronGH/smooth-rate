4d3a1e464a74ba5dfc843d6a21ebf45e06f6eb69
peron <peron_ap@icloud.com>
2023-06-05 11:27:13 +0800

    feat(rate-limiter): clarify variable names in rate limiter
    This commit improves code readability by renaming several variables in the RateLimiter class and related interfaces. It also adjusts the corresponding Lua scripts for clarity.
    
    - `window` has been renamed to `windowMs` to specify the unit is in milliseconds.
    - `prefix` has been renamed to `keyPrefix` to reflect its purpose more accurately.
    - `maxRate` has been renamed to `maxRequests` to better represent its role.
    - Variable names in Lua scripts have been adjusted to match the TypeScript changes.
    
    The change helps in maintaining a clear and consistent codebase, making it easier for others to understand the function and purpose of these variables.

--- src/limiter.ts
+++ src/limiter.ts
@@ -3,75 +3,77 @@ interface EvalFunction {
 }
 
 export interface RateLimiterConfig {
-  prefix: string;
-  window: number;
-  maxRate: number;
+  keyPrefix: string;
+  windowMs: number;
+  maxRequests: number;
   evalFunc: EvalFunction;
 }
 
 export interface RateLimitInfo {
   isLimited: boolean;
-  nextAvailableTime: number;
-  remaining: number;
+  nextAvailableTimestamp: number;
+  remainingRequests: number;
 }
 
 export class RateLimiter {
-  private prefix: string;
-  private window: number;
-  private maxRate: number;
+  private keyPrefix: string;
+  private windowMs: number;
+  private maxRequests: number;
   private evalFunc: EvalFunction;
 
   private static limitScript = `
-    local current = tonumber(ARGV[1])
-    local window = tonumber(ARGV[2])
-    local limit = tonumber(ARGV[3])
+    local currentTimestamp = tonumber(ARGV[1])
+    local windowMs = tonumber(ARGV[2])
+    local maxRequests = tonumber(ARGV[3])
 
-    redis.call('ZREMRANGEBYSCORE', KEYS[1], '-inf', current - window)
+    redis.call('ZREMRANGEBYSCORE', KEYS[1], '-inf', currentTimestamp - windowMs)
 
-    local remaining = limit - redis.call('ZCARD', KEYS[1])
-    local next_available_time = redis.call('ZRANGE', KEYS[1], 0, 0)[1] + window
+    local remainingRequests = maxRequests - redis.call('ZCARD', KEYS[1])
+    local nextAvailableTimestamp = redis.call('ZRANGE', KEYS[1], 0, 0)[1] + windowMs
 
-    if remaining > 0 then
-      redis.call('ZADD', KEYS[1], current, current)
-      redis.call('EXPIRE', KEYS[1], window / 1000)
+    if remainingRequests > 0 then
+      redis.call('ZADD', KEYS[1], currentTimestamp, currentTimestamp)
+      redis.call('EXPIRE', KEYS[1], windowMs / 1000)
     end
 
-    return tostring(remaining) .. ',' .. tostring(next_available_time) .. ',' .. tostring(remaining <= 0)
+    return tostring(remainingRequests) .. ',' .. tostring(nextAvailableTimestamp) .. ',' .. tostring(remainingRequests <= 0)
   `;
 
   private static checkScript = `
-    local current = tonumber(ARGV[1])
-    local window = tonumber(ARGV[2])
-    local limit = tonumber(ARGV[3])
+    local currentTimestamp = tonumber(ARGV[1])
+    local windowMs = tonumber(ARGV[2])
+    local maxRequests = tonumber(ARGV[3])
 
-    redis.call('ZREMRANGEBYSCORE', KEYS[1], '-inf', current - window)
+    redis.call('ZREMRANGEBYSCORE', KEYS[1], '-inf', currentTimestamp - windowMs)
 
-    local remaining = limit - redis.call('ZCARD', KEYS[1])
-    local next_available_time = redis.call('ZRANGE', KEYS[1], 0, 0)[1] + window
+    local remainingRequests = maxRequests - redis.call('ZCARD', KEYS[1])
+    local nextAvailableTimestamp = redis.call('ZRANGE', KEYS[1], 0, 0)[1] + windowMs
 
-    return tostring(remaining) .. ',' .. tostring(next_available_time) .. ',' .. tostring(remaining <= 0)
+    return tostring(remainingRequests) .. ',' .. tostring(nextAvailableTimestamp) .. ',' .. tostring(remainingRequests <= 0)
   `;
 
   constructor(config: RateLimiterConfig) {
-    this.prefix = config.prefix;
-    this.window = config.window;
-    this.maxRate = config.maxRate;
+    this.keyPrefix = config.keyPrefix;
+    this.windowMs = config.windowMs;
+    this.maxRequests = config.maxRequests;
     this.evalFunc = config.evalFunc;
   }
 
   private async runScript(script: string, key: string): Promise<RateLimitInfo> {
-    const now = Date.now();
-    const namespacedKey = `${this.prefix}:${key}`;
+    const currentTimestamp = Date.now();
+    const namespacedKey = `${this.keyPrefix}:${key}`;
     const result = await this.evalFunc(script, [namespacedKey], [
-      now.toString(),
-      this.window.toString(),
-      this.maxRate.toString(),
+      currentTimestamp.toString(),
+      this.windowMs.toString(),
+      this.maxRequests.toString(),
     ]);
 
-    const [remaining, nextAvailableTime, isLimited] = result.split(",");
+    const [remainingRequests, nextAvailableTimestamp, isLimited] = result.split(
+      ",",
+    );
     return {
-      remaining: parseInt(remaining),
-      nextAvailableTime: parseInt(nextAvailableTime),
+      remainingRequests: parseInt(remainingRequests),
+      nextAvailableTimestamp: parseInt(nextAvailableTimestamp),
       isLimited: isLimited === "true",
     };
   }
